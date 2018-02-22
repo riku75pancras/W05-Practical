6de331e0b7233d8f0b61996e6ca127eba7cf7d02
ry26 <riku75pancras'gmail.com>
2018-02-22 22:00:32 +0000

    \just added

--- src/SimilarityScores.java
+++ src/SimilarityScores.java
@@ -1,28 +1,34 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimilarityScores {
-    ArrayList<ArrayList<String>> bigramList = new ArrayList<ArrayList<String>>();
-    bigramList.add(new ArrayList<String>());
 
     ArrayList<String> bigramListForWord = new ArrayList<String>();
+    private int lineCounter;
 
-    public void calculateSimilarityScore(String filepath, String queryWord){
+    public void printSimilarityScore(String filepath, String queryWord) throws IOException {
         SentenceReader sentenceReader = new SentenceReader();
-        createBigramsForSentence(sentenceReader.readAllSentences(filepath));
-        createBigramsForQuery(queryWord);
+        System.out.println(createBigramsForSentence(sentenceReader.readAllSentences(filepath)));
+        //createBigramsForQuery(queryWord);
+
     }
 
-    private ArrayList<ArrayList<String>> createBigramsForSentence(ArrayList<String> sentencesList){
-        for(String s : sentencesList){
-            for(int i = 0; i < sentencesList.size(); i++) {
-                for (int j = 0; j < (s.length() - 1); j++) {
-                    bigramList.get(i).add(s.substring(j, j + 1));
+    private ArrayList<ArrayList<String>> createBigramsForSentence(List<String> sentencesList){
+        ArrayList<ArrayList<String>> bigramList = new ArrayList<>();
+        bigramList.add(new ArrayList<String>());
+
+            while(lineCounter < sentencesList.size()) {
+                    for (String s : sentencesList) {
+                        if (s != "") {
+                            for (int j = 0; j < (s.length() - 1); j++) {
+                                bigramList.get(lineCounter).add(s.substring(j, j + 1));
+                            }
+                            lineCounter += 1;
+                        }
+                    }
                 }
-            }
-        }
+        
         return bigramList;
     }
 
@@ -30,6 +36,8 @@ public class SimilarityScores {
         for (int i = 0; i < (queryWord.length() - 1); i++){
             bigramListForWord.add(queryWord.substring(i, i + 1));
         }
+
+        return  bigramListForWord;
     }
 
 }
--- src/W05Practical.java
+++ src/W05Practical.java
@@ -1,17 +1,19 @@
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class W05Practical {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        BufferedReader reader = new BufferedReader(new FileReader(args[0]))){
-          try {
-              String filePath = args[0];
-              SimilarityScores similarityScore = new SimilarityScores();
-              similarityScore.calculateSimilarityScore(filePath, args[1]);
-          } catch (IOException e) {
+        try {
+            String filePath = args[0];
+            SimilarityScores similarityScore = new SimilarityScores();
+            similarityScore.printSimilarityScore(filePath, args[1]);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
     }
